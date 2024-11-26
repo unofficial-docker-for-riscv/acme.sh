5e982557b8590430137a02ea5a6f1d0c15493be3
ngc7331 <ngc7331@outlook.com>
2024-11-26 19:28:41 +0800

    fix(docker-for-riscv): tags & labels fix

--- build-docker-riscv.sh
+++ build-docker-riscv.sh
@@ -1,35 +1,25 @@
 #!/bin/bash
 # DO NOT UPSTREAM THIS FILE
 
-GITHUB_REF=${GITHUB_REF:-refs/heads/master}
-
-if [[ $GITHUB_REF == refs/tags/* ]]; then
-  DOCKER_IMAGE_TAG=${GITHUB_REF#refs/tags/}
-fi
-
-if [[ $GITHUB_REF == refs/heads/* ]]; then
-  DOCKER_IMAGE_TAG=${GITHUB_REF#refs/heads/}
-
-  if [[ $DOCKER_IMAGE_TAG == master ]]; then
-    DOCKER_IMAGE_TAG=latest
-    AUTO_UPGRADE=1
-  fi
-fi
+VERSION=$(cat ./acme.sh | grep 'VER=' | cut -d '=' -f 2 | tr -d '"')
 
 REPOS=(${REPOS:-ngc7331/acme.sh})
 TAGS=()
 for repo in ${REPOS[@]}; do
-	TAGS+=("-t ${repo,,}:${DOCKER_IMAGE_TAG}")
-    TAGS+=("-t ${repo,,}:latest")
+  TAGS+=("-t ${repo,,}:${VERSION}")
+  TAGS+=("-t ${repo,,}:latest")
 done
 
 DOCKER_LABELS=()
-while read -r label; do
-  DOCKER_LABELS+=(--label "${label}")
-done <<<"${DOCKER_METADATA_OUTPUT_LABELS}"
+if [ -n "${DOCKER_METADATA_OUTPUT_LABELS}" ]; then
+  while read -r label; do
+    DOCKER_LABELS+=(--label "${label}")
+  done <<<"${DOCKER_METADATA_OUTPUT_LABELS}"
+fi
 
 docker buildx build \
   ${TAGS[@]} \
+  "${DOCKER_LABELS[@]}" \
   --push \
   --build-arg AUTO_UPGRADE=${AUTO_UPGRADE} \
   --platform linux/riscv64 .
